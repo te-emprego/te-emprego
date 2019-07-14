d51a27f801d9e515140ec2ff3f899fbeba9173ec
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-07-14 20:25:44 -0300

    add login screen, style and logic at same file

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Header from './Header/Header.component'
-import background from '../images/background.svg'
-import Container from './Global/Container.style';
-import Main from './Global/Main.style';
-import Sidebar from './Sidebar/Sidebar.component'
-import JobList from './JobList/JobList.component';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Header from "./Header/Header.component";
+import background from "../images/background.svg";
+import Container from "./Global/Container.style";
+import Main from "./Global/Main.style";
+import Sidebar from "./Sidebar/Sidebar.component";
+import JobList from "./JobList/JobList.component";
 
 const Root = styled.div`
-  transition: .25s ease;
-  min-height: 100vh;
-  background-image: url(${background});
-  background-repeat: no-repeat;
-  background-position: center top;
-`
+	transition: .25s ease;
+	min-height: 100vh;
+	background-image: url(${background});
+	background-repeat: no-repeat;
+	background-position: center top;
+`;
 
 const MainContainer = styled(Container)`
   display: flex;
@@ -25,22 +25,22 @@ const MainContainer = styled(Container)`
   >div:last-child {
     width: 320px;
   }
-`
+`;
 
 class App extends Component {
-  render() {
-    return(
-      <Root>
-        <Header />
-        <MainContainer>
-          <Main>
-            <JobList/>
-          </Main>
-          <Sidebar/>
-        </MainContainer>
-      </Root>
-    )
-  }
+	render () {
+		return (
+			<Root>
+				<Header />
+				<MainContainer>
+					<Main>
+						<JobList />
+					</Main>
+					<Sidebar />
+				</MainContainer>
+			</Root>
+		);
+	}
 }
 
-export default App
+export default App;
