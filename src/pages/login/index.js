70156acde7eff4895de5be188ec2ebbca1d9b599
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-07-15 18:52:20 -0300

    fix identation

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import MaterialIcon from 'material-icons-react';
 import colors from '../../globalStyles/colors';
 
-import { Root, HeaderLogin, RightEffect, LoginForm, SocialLinks, BottomEffect } from './styles';
+import {
+  Root,
+  HeaderLogin,
+  RightEffect,
+  LoginForm,
+  SocialLinks,
+  BottomEffect,
+} from './styles';
 import logo from '../../assets/images/Logo.svg';
 
 import right from '../../assets/images/fx.svg';
@@ -11,51 +18,51 @@ import linkedin from '../../assets/images/linkedin.svg';
 import github from '../../assets/images/github.svg';
 
 const Login = () => (
-	<Root>
-		<HeaderLogin>
-			<img src={logo} alt="Logo Te Emprego" />
-		</HeaderLogin>
-		<RightEffect>
-			<img src={right} alt="right effect" />
-		</RightEffect>
+  <Root>
+    <HeaderLogin>
+      <img src={logo} alt="Logo Te Emprego" />
+    </HeaderLogin>
+    <RightEffect>
+      <img src={right} alt="right effect" />
+    </RightEffect>
 
-		<LoginForm>
-			<h1>Login</h1>
-			<p>Bem-vindo novamente! </p>
+    <LoginForm>
+      <h1>Login</h1>
+      <p>Bem-vindo novamente! </p>
 
-			<form>
-				<input type="text" placeholder="email" />
-				<input type="password" placeholder="senha" />
-				<button type="submit">
-					<MaterialIcon icon="arrow_forward" color={colors.darken} />
-					<span>Entrar</span>
-				</button>
-			</form>
-			<a href="/">esqueci minha senha</a>
-		</LoginForm>
+      <form>
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="senha" />
+        <button type="submit">
+          <MaterialIcon icon="arrow_forward" color={colors.darken} />
+          <span>Entrar</span>
+        </button>
+      </form>
+      <a href="/">esqueci minha senha</a>
+    </LoginForm>
 
-		<SocialLinks>
-			<p>ou invés disso, faça login com</p>
-			<div>
-				<button>
-					<img src={linkedin} alt="Linkedin" />
-					<span>LinkedIn</span>
-				</button>
-				<button>
-					<img src={github} alt="Linkedin" />
-					<span>GitHub</span>
-				</button>
-			</div>
-		</SocialLinks>
+    <SocialLinks>
+      <p>ou invés disso, faça login com</p>
+      <div>
+        <button type="button">
+          <img src={linkedin} alt="LinkedIn" />
+          <span>LinkedIn</span>
+        </button>
+        <button type="button">
+          <img src={github} alt="GitHub" />
+          <span>GitHub</span>
+        </button>
+      </div>
+    </SocialLinks>
 
-		<p>
-			Não tem uma conta? <a href="/">Crie uma</a>
-		</p>
+    <p>
+      Não tem uma conta? <a href="/">Crie uma</a>
+    </p>
 
-		<BottomEffect>
-			<img src={bottom} alt="bottom effect" />
-		</BottomEffect>
-	</Root>
+    <BottomEffect>
+      <img src={bottom} alt="bottom effect" />
+    </BottomEffect>
+  </Root>
 );
 
 export default Login;
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -2,158 +2,158 @@ import styled from 'styled-components';
 import colors from '../../globalStyles/colors';
 
 export const Root = styled.div`
-	background: linear-gradient(${colors.darken}, ${colors.primary});
-	height: 100vh;
-	position: relative;
-	overflow: hidden;
-	text-align: center;
+  background: linear-gradient(${colors.darken}, ${colors.primary});
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
 
-	p,
-	a {
-		color: ${colors.white};
-	}
+  p,
+  a {
+    color: ${colors.white};
+  }
 
-	a {
-		text-decoration: none;
-		font-weight: bold;
-	}
+  a {
+    text-decoration: none;
+    font-weight: bold;
+  }
 `;
 
 export const HeaderLogin = styled.div`
-	padding: 100px 80px 20px 80px;
-	text-align: left;
+  padding: 100px 80px 20px 80px;
+  text-align: left;
 `;
 
 export const LoginForm = styled.div`
-	width: 355px;
-	margin: auto;
-	text-align: center;
-	margin-top: 50px;
+  width: 355px;
+  margin: auto;
+  text-align: center;
+  margin-top: 50px;
 
-	h1,
-	p {
-		text-shadow: 0 2px 5px ${colors.shadowText};
-		color: ${colors.white};
-	}
+  h1,
+  p {
+    text-shadow: 0 2px 5px ${colors.shadowText};
+    color: ${colors.white};
+  }
 
-	h1 {
-		text-transform: uppercase;
-		font-size: 3.0rem;
-		font-family: "Raleway", sans-serif;
-	}
+  h1 {
+    text-transform: uppercase;
+    font-size: 3rem;
+    font-family: 'Raleway', sans-serif;
+  }
 
-	a {
-		font-size: 0.8rem;
-		color: ${colors.white};
-		text-decoration: none;
-	}
+  a {
+    font-size: 0.8rem;
+    color: ${colors.white};
+    text-decoration: none;
+  }
 
-	form {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-		input {
-			width: 80%;
-			padding: 10px;
-			margin: 20px 0;
-			border: none;
-			border-bottom: 1px solid ${colors.white};
-			background: transparent;
-			outline: none;
-			color: ${colors.white};
-			font-family: "Nunito", sans-serif;
+    input {
+      width: 80%;
+      padding: 10px;
+      margin: 20px 0;
+      border: none;
+      border-bottom: 1px solid ${colors.white};
+      background: transparent;
+      outline: none;
+      color: ${colors.white};
+      font-family: 'Nunito', sans-serif;
 
-			&::placeholder {
-				color: ${colors.white};
-				font-family: "Nunito", sans-serif;
-				font-size: 1.1rem;
-			}
-		}
+      &::placeholder {
+        color: ${colors.white};
+        font-family: 'Nunito', sans-serif;
+        font-size: 1.1rem;
+      }
+    }
 
-		button {
-			width: 80%;
-			margin: 15px 0;
-			background: ${colors.white};
-			border: none;
-			border-radius: 10px;
-			font-size: 0.975rem;
-			padding: 10px 20px;
-			font-family: "Nunito", sans-serif;
-			-webkit-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
-			-moz-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
-			box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			vertical-align: middle;
-			cursor: pointer;
-			color: #8f54f8;
-			transition: .3s ease;
+    button {
+      width: 80%;
+      margin: 15px 0;
+      background: ${colors.white};
+      border: none;
+      border-radius: 10px;
+      font-size: 0.975rem;
+      padding: 10px 20px;
+      font-family: 'Nunito', sans-serif;
+      -webkit-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+      -moz-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+      box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      vertical-align: middle;
+      cursor: pointer;
+      color: #8f54f8;
+      transition: 0.3s ease;
 
-			span {
-				text-transform: uppercase;
-				font-weight: bold;
-				padding: 0 10px;
-			}
-		}
-	}
+      span {
+        text-transform: uppercase;
+        font-weight: bold;
+        padding: 0 10px;
+      }
+    }
+  }
 `;
 
 export const SocialLinks = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin: 35px 0;
+  display: flex;
+  flex-direction: column;
+  margin: 35px 0;
 
-	p {
-		color: ${colors.white};
-		text-transform: uppercase;
-		font-size: 0.775rem;
-	}
+  p {
+    color: ${colors.white};
+    text-transform: uppercase;
+    font-size: 0.775rem;
+  }
 
-	div {
-		margin: 0 auto;
-		display: flex;
-		margin-top: 10px;
-		color: ${colors.darken};
+  div {
+    margin: 0 auto;
+    display: flex;
+    margin-top: 10px;
+    color: ${colors.darken};
 
-		button {
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			vertical-align: middle;
-			border: none;
-			padding: 5px 7px;
-			margin: 0 10px;
-			background: ${colors.white};
-			border-radius: 5px;
-			cursor: pointer;
-			-webkit-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
-			-moz-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
-			box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      vertical-align: middle;
+      border: none;
+      padding: 5px 7px;
+      margin: 0 10px;
+      background: ${colors.white};
+      border-radius: 5px;
+      cursor: pointer;
+      -webkit-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+      -moz-box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
+      box-shadow: 0px 2px 2px 0px ${colors.shadowBox};
 
-			span {
-				margin-left: 5px;
-			}
-		}
-	}
+      span {
+        margin-left: 5px;
+      }
+    }
+  }
 `;
 
 export const RightEffect = styled.div`
-	img {
-		position: absolute;
-		top: 0;
-		right: 0;
-		width: 35%;
-	}
+  img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 35%;
+  }
 `;
 
 export const BottomEffect = styled.div`
-	width: 100%;
-	margin: 0 auto;
-	img {
-		position: absolute;
-		bottom: 0;
-		left: calc(50% - 250px);
-	}
+  width: 100%;
+  margin: 0 auto;
+  img {
+    position: absolute;
+    bottom: 0;
+    left: calc(50% - 250px);
+  }
 `;
