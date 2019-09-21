1d86daa20f1aaaabe8321991f7a809703b45c537
ederrr <ederrosate@yahoo.com.br>
2019-09-20 23:59:55 -0300

    corrigida altura do overlay

--- src/components/Overlay/styles.js
+++ src/components/Overlay/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 export const OverlayWrapper = styled.div`
   background-color: black;
-  min-width: 100%;
-  height: 100%;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
   z-index: 100;
   display: block;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   opacity: 0.7;
